e204a28649f19028ef9b9707820a53cd38ef4a5b
Diego Bonilla <drbv27@gmail.com>
2022-03-27 13:37:33 -0500

    Some styles and link to home into 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,7 @@
 import Layout from "../components/Layout";
+import Link from "next/dist/client/link";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
 
 
 const Custom404 = () => (
@@ -6,6 +9,11 @@ const Custom404 = () => (
         <div className="container text-center mt-5 text-info">
             <h1>404</h1>
             <h4>This page does not exist!!!</h4>
+            <p>
+            <Link href="/">
+            <a className="text-info">Home <FontAwesomeIcon icon={faHouse} /></a>
+          </Link>
+            </p>
         </div>
     </Layout>
 )
